e06f12e0ee0bb157f1ad326fdaaf1d22a158c00f
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-05-02 01:19:26 +0600

    Update redis config file

--- src/v1/configs/redis.config.js
+++ src/v1/configs/redis.config.js
@@ -1,15 +1,5 @@
 require('dotenv').config();
-const {
-  REDIS_URL,
-  REDIS_HOST,
-  REDIS_PORT,
-  REDIS_USERNAME,
-  REDIS_PASSWORD,
-} = require('../environments');
+const { REDIS_URL } = require('../environments');
 module.exports = {
   url: REDIS_URL,
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-  username: REDIS_USERNAME,
-  password: REDIS_PASSWORD,
 };
