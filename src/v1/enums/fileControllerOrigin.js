e9f2c7c4e3d7dd23dc4352714d9faad159693f14
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-05-02 01:20:45 +0600

    Add storFile key in fileController error origin enum

--- src/v1/enums/fileControllerOrigin.js
+++ src/v1/enums/fileControllerOrigin.js
@@ -2,6 +2,7 @@ const FileControllerOrigin = {
   upload: 'upload-base-error',
   getFile: 'get-file-base-error',
   deleteFile: 'delete-file-base-error',
+  storeFile: 'store-file-base-error',
 };
 
 module.exports = FileControllerOrigin;
