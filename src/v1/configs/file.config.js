e3324f96999ecb01523162f0cb3959f7402473ae
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-05-02 01:19:50 +0600

    Update file config

--- src/v1/configs/file.config.js
+++ src/v1/configs/file.config.js
@@ -1,17 +1,13 @@
 const {
-  FOLDER,
-  BUCKET_NAME,
   PROVIDER,
+  CONFIG,
   FORM_DATA_KEY,
   MAX_FILE_SIZE,
-  MAX_FILES,
 } = require('../environments');
 
 module.exports = {
-  folder: FOLDER,
-  bucketName: BUCKET_NAME,
   provider: PROVIDER,
+  config: CONFIG,
   formDataKey: FORM_DATA_KEY,
   maxFileSize: MAX_FILE_SIZE,
-  maxFiles: MAX_FILES,
 };
