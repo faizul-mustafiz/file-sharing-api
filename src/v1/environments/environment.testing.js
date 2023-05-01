dca4ed53001f7e6c47e324189caef205d03a7cbc
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-05-01 16:40:30 +0600

    Add dev, prod, stage and test environment variable exporter module

---
+++ src/v1/environments/environment.testing.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+module.exports = {
+  //app environment variables
+  API_PROTOCOL: process.env.API_PROTOCOL,
+  API_HOST: process.env.API_HOST,
+  API_PORT: process.env.API_PORT,
+  BASE_API_ROUTE: process.env.BASE_API_ROUTE,
+
+  // redis environments variables
+  REDIS_URL: process.env.REDIS_URL_TEST,
+  REDIS_HOST: process.env.REDIS_HOST,
+  REDIS_PORT: process.env.REDIS_PORT,
+  REDIS_USERNAME: process.env.REDIS_USERNAME,
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+
+  // file storage environment variables
+  FOLDER: process.env.FOLDER,
+  BUCKET_NAME: process.env.BUCKET_NAME,
+  PROVIDER: process.env.PROVIDER,
+  FORM_DATA_KEY: process.env.FORM_DATA_KEY,
+  MAX_FILE_SIZE: process.env.MAX_FILE_SIZE,
+  MAX_FILES: process.env.MAX_FILES,
+};
